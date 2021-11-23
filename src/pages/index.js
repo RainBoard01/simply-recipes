81da62b4a07ae6b701d1cb31517095a93fab0810
rainboard01 <amdmsi4ever@hotmail.com>
2021-11-23 17:50:08 -0300

    Installed chakra-ui and emotion and started to make navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<ChakraProvider>
+			<Navbar />
+		</ChakraProvider>
+	);
 }
