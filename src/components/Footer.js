1fc0c822798071543657593a485c5a8fe1a60952
rainboard01 <amdmsi4ever@hotmail.com>
2021-11-23 20:29:19 -0300

    Add navbar with styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
+import { Center, Text } from '@chakra-ui/react';
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Span = styled.span`
+	color: #645cff;
+`;
 
 const Footer = () => {
 	return (
 		<footer>
-			<h2>hello from footer</h2>
+			<Center bg='black' w='100%' h='72px' p='4' color='white'>
+				<Text fontSize='lg'>
+					© 2021 <Span>SimplyRecipes</Span>. Built with{' '}
+					<Span>
+						<a href='https://www.gatsbyjs.com/'>Gatsby</a>
+					</Span>
+				</Text>
+			</Center>
 		</footer>
 	);
 };
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,20 @@
+import { Center, Heading } from '@chakra-ui/react';
 import React from 'react';
 import Layout from '../components/Layout';
 
 const Error = () => {
-	return <Layout>Error 404 page not exist</Layout>;
+	return (
+		<Layout>
+			<Center>
+				<Heading as='h1' size='4xl'>
+					404
+				</Heading>
+				<Heading as='h2' size='xl'>
+					page not found
+				</Heading>
+			</Center>
+		</Layout>
+	);
 };
 
 export default Error;
